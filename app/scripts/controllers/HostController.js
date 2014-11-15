3ba8c7b7394cf816b1429a38d3bf8af16d34e2e2
unknown <neemathu@NEEMATHU-LAP.in.oracle.com>
2014-11-15 19:19:03 +0530

    added host files

--- app/scripts/controllers/HostController.js
+++ app/scripts/controllers/HostController.js
@@ -1,4 +1,13 @@
-module.controller('HostController', function ($scope,$http, HostService) {
+module.controller('HostController', function ($scope,$http, HostService,docker) {
  
+	$scope.loadInfo= function () {
+		HostService.getInfo(function(data) {
+			 $scope.info = data;
+			 $scope.info.serverurl=docker.baseurl;
+		  });
+	  }
+
+	$scope.loadInfo();
+	
 	
 });
--- app/scripts/services/HostService.js
+++ app/scripts/services/HostService.js
@@ -1,3 +1,10 @@
 module.service('HostService', function ($http,docker) {
 	
+	  //simply returns the host information
+	return {
+     getInfo: function(callback) {
+       $http.get(docker.baseurl+'/info').success(callback);
+     }
+   }
+	
 });
